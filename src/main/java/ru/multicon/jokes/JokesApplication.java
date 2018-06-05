53e26823a87188c28c238acba0edba1b813727c5
Max Samkov <littlebiggame2@gmail.com>
2018-06-05 16:45:01 +0300

    Spring XML Configuration bean

--- src/main/java/ru/multicon/jokes/JokesApplication.java
+++ src/main/java/ru/multicon/jokes/JokesApplication.java
@@ -2,9 +2,11 @@ package ru.multicon.jokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.ImportResource;
+
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokesApplication {
 
 	public static void main(String[] args) {
